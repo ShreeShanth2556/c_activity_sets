6d76e06090b6ce93d42cdfaf6758ba2f83871fce
Shreeshanth <shrees.bsc23@rvu.edu.in>
2024-02-06 14:01:37 +0530

    text

--- set01/problem07.c
+++ set01/problem07.c
@@ -1,1 +1,36 @@
 //Write a C program to find sum of all natural numbers until _n_
+#include<stdio.h>
+int input_n();
+int sum_n_nos(int n);
+void output(int n, int sum);
+
+int input_n()
+{
+    int n;
+    printf("Enter the value\n");
+    scanf("%d",&n);
+    return n;
+}
+
+int sum_n_nos(int n)
+{
+    int sum=0;
+    for(int i=1;i<=n;i++)
+    {
+        sum=sum+i;
+    }
+}
+
+void output(int n, int sum)
+{
+    printf("The sum of %d and %d is %d",n,sum);
+}
+
+int main()
+{
+    int n,sum;
+    n=input_n();
+    sum=sum_n_nos(n);
+    output(n,sum);
+    return 0;
+}
