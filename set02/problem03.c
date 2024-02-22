b0646d0bbde0750b980d8f75679caa380d01bb4a
Shreeshanth <shrees.bsc23@rvu.edu.in>
2024-02-22 21:21:25 +0530

    test

--- set02/problem03.c
+++ set02/problem03.c
@@ -16,5 +16,32 @@ input_number()
 
 int is_composite(int n)
 {
-    
+    int i;
+    for(i=2;i<n;i++)
+    {
+        if(n%i==0)
+        return i;
+    }
+    return 0;
+}
+
+void output(int n, int result)
+{
+    if(result==1)
+    {
+        printf("the number is composite");
+    }
+    else
+    {
+        printf("the number is not composite");
+    }
+}
+
+int main()
+{
+    int n,result;
+    n=input_number();
+    result=is_composite(n);
+    output(n,result);
+    return 0;
 }
