56d2066619343320c20f70d2e3337afb009f0f45
Shreeshanth <shrees.bsc23@rvu.edu.in>
2024-03-04 14:03:43 +0530

    test

--- set03/problem02.c
+++ set03/problem02.c
@@ -43,6 +43,6 @@ void output(float x1, float y1, float x2, float y2,float x3, float y3,int result
     }
 else
     {
-        printf("the points (%f,%f),(%f,%f) and (%f,%f)  do not form a triangle",x1,y1,x2,y2,x3,y3);
+        printf("The points (%f,%f),(%f,%f) and (%f,%f)  do not form a triangle",x1,y1,x2,y2,x3,y3);
     }
 }
